ba9d345d93082e5cb23f3aea989b38f72f9074d2
S <s>
2016-06-29 10:44:18 +0800

    {"types":"C","runs":9}

--- IllusoryContours.java
+++ IllusoryContours.java
@@ -46,7 +46,10 @@ public class IllusoryContours extends GraphicsProgram {
 		circle3.setColor(Color.BLACK);
 		circle4.setFilled(true);
 		circle4.setColor(Color.BLACK);
-		
+		add(circle1);
+		add(circle2);
+		add(circle3);
+		add(circle4);
 	}
 
 	private void showRect() {
