e5ac1af57032473940a2091424017c346926f528
S <s>
2016-06-29 11:46:03 +0800

    {"types":"C","runs":55}

--- Pawprints.java
+++ Pawprints.java
@@ -54,10 +54,10 @@ public class Pawprints extends GraphicsProgram {
 	 * @param y The y coordinate of the upper-left corner of the bounding box for the pawprint.
 	 */
 	private void drawPawprint(double x, double y) {
-		drawOval(x+FIRST_TOE_OFFSET_X,y+FIRST_TOE_OFFSET_Y,TOE_WIDTH,TOE_HEIGHT);
-		drawOval(x+SECOND_TOE_OFFSET_X,y+SECOND_TOE_OFFSET_Y,TOE_WIDTH,TOE_HEIGHT);
-		drawOval(x+THIRD_TOE_OFFSET_X,y+THIRD_TOE_OFFSET_Y,TOE_WIDTH,TOE_HEIGHT);
-		drawOval(x+HEEL_OFFSET_X,y+HEEL_OFFSET_Y,HEEL_WIDTH,HEEL_HEIGHT);
+		drawToe(x+FIRST_TOE_OFFSET_X,y+FIRST_TOE_OFFSET_Y,TOE_WIDTH,TOE_HEIGHT);
+		drawToe(x+SECOND_TOE_OFFSET_X,y+SECOND_TOE_OFFSET_Y,TOE_WIDTH,TOE_HEIGHT);
+		drawToe(x+THIRD_TOE_OFFSET_X,y+THIRD_TOE_OFFSET_Y,TOE_WIDTH,TOE_HEIGHT);
+		drawHeel(x+HEEL_OFFSET_X,y+HEEL_OFFSET_Y,HEEL_WIDTH,HEEL_HEIGHT);
 	}
 
 	private void drawFirstToe(double d, double e, double toeWidth, double toeHeight) {
