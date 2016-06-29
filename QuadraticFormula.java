cd02945a55d8ee392031a59b1208b2fe93f41f2a
S <s>
2016-06-29 09:20:28 +0800

    {"types":"C","runs":2}

--- QuadraticFormula.java
+++ QuadraticFormula.java
@@ -1,10 +1,33 @@
-/* TODO: Replace these file comments with a description of what your program
- * does.
+/* TODO: A program that the roots of the quadratic equation
  */
 import acm.program.*;
 
 public class QuadraticFormula extends ConsoleProgram {
+	public int a,b,c;
+	public double discriminant;
 	public void run() {
+		getNumber();
+		getRoots();
+	}
+
+	private void getNumber() {
+		println("This program needs three intengers:");
+		a = readInt("Enter first int a:");
+		b = readInt("Enter second int b:");
+		c = readInt("Enter third int c:");
+	}
+
+	private void getRoots() {
+		getDiscriminant();
+		showRoots();
+	}
+
+	private void getDiscriminant() {
+		discriminant = Math.sqrt(b*b - 4*a*c);
+	}
+
+	private void showRoots() {
+		// TODO Auto-generated method stub
 		
 	}
 }
