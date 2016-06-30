1f6cdb03d420960de9832ea1c21eec88bb89a588
S <s>
2016-06-30 22:39:43 +0800

    {"types":"C","runs":2}

--- BoxesIllusion.java
+++ BoxesIllusion.java
@@ -23,6 +23,8 @@ public class BoxesIllusion extends GraphicsProgram {
 	public static final int APPLICATION_WIDTH = 350;
 	public static final int APPLICATION_HEIGHT = 300;
 
+	public static final int BLANK_WIDTH = 20;
+	public static final int BLANK_HEIGHT = 20;
 	public void run() {
 		/*
 		 * use a double loop to create the boxes illusion 
