f7eddb2a99c7f44c1f76d05139932c44c76a7a36
S <s>
2016-06-30 22:56:30 +0800

    {"types":"C","runs":4}

--- Caterpillar.java
+++ Caterpillar.java
@@ -13,6 +13,33 @@ public class Caterpillar extends GraphicsProgram {
 	 * Remember to add comments describing what each constant does!
 	 */	
 	
+	
+	//Tht radius of the segment circle
+	private static final int SEGMENT_RADIUS = 30;
+	
+	//The width of the near segments;
+	private static final int WIDTH = 30;
+	
+	//The (x,y) of the left-up coordinate of the first segment.
+	private static final int x = 0;
+	private static final int y = 30;
+	
+	private static final int x1 = 30;
+	private static final int y1 = 0;
+	
+	private static final int x2 = x + 30;
+	private static final int y2 = y;
+	
+	private static final int x3 = 30;
+	private static final int y3 = 0;
+	
+	private static final int x4 = x2 + 30;
+	private static final int y4 = y;
+	
+	private static final int x5 = 30;
+	private static final int y5 = 0;
+	
+
 	public void run() {
 		
 	}
